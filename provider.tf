8b05ee12149d9667247385bda1aef7b26022b625
vinodkumarvaishncr <vinodkumarvaishncr@gmail.com>
2025-10-13 10:30:34 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,20 +2,20 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.35.0"
+      version = "4.47.0"
     }
   }
 
   backend "azurerm" {
-    resource_group_name  = "vinod-main-rg"
-    storage_account_name = "storagemainvinod"
-    container_name       = "vinodcontainer"
-    key                  = "infra-dev-key.tfstate"
+    resource_group_name  = "vinod_main_rg"
+    storage_account_name = "vinodstorgeforstatefile"
+    container_name       = "vinodcontainerforstatefile"
+    key                  = "manual.terraform.tfstate"
   }
    
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "98b7163f-e961-4b65-98c2-7a6dc2edf5b7"
+  subscription_id = "239a3096-3fb0-4f7c-8060-6127b0f8808e"
 }
